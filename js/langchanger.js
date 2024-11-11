8dbbca10a49ea5df98996842ecea19ac6b46c981
iliass-wakkar <iliasswakkar.wip@gmail.com>
2024-11-11 11:40:44 +0100

    commit

---
+++ js/langchanger.js
@@ -0,0 +1,8 @@
+const aboutMeTitle = document.getElementById('aboutMeTitle');
+const fullName=document.getElementById("fullName");
+const phone=document.getElementById("phone");
+const email=document.getElementById("email");
+const aboutMeText=document.getElementById("aboutMeText");
+const resumeTitle=document.getElementById("resumeTitle");
+const experienceTitle=document.getElementById("experienceTitle");
+
